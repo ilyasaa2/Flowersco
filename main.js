c090d6ee157fadd678b6cbff2d2021d3c6024a12
muhammadramdhan <mandzonswag@gamail.com>
2026-03-11 20:41:10 +0700

    udah tak hapus sama nambahin sedikit

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const loginBtn = document.getElementById("loginbtn");
+
+  loginBtn.addEventListener("click", function () {
+    window.location.href = "Login.html";
+  });
+
+});
+
